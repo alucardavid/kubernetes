3d0cd4756d8ceafdf4c5c61c0e9a48d917a8eb10
David Pereira <d_pereira@outlook.com.br>
2019-05-19 20:21:42 -0300

    Creating replication controller with yaml file

--- app.js
+++ app.js
@@ -10,15 +10,8 @@ console.log(`Kubia server starting...`)
 
 app.get('/*', (req, res, next) => {
     console.log(`Received request from ${req.connection.remoteAddress}`)
-    countReq++
-    if (countReq > 5 ) {
-        res.status(400)
-        res.end(`Requisitions above the limit`)
-    }
-    else {
-        res.status(200)
-        res.end(`You've hit ${os.hostname()} \n Req count: ${countReq}\n`)
-    }
+    res.status(200)
+    res.end(`You've hit ${os.hostname()} \n Req count: ${countReq}\n`)
 })
 
 app.listen(8080, () => {
