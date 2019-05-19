6e8705bafef043a49f4e3efb201eaad15b2779be
David Pereira <d_pereira@outlook.com.br>
2019-05-19 17:55:30 -0300

    Creating Liveness probe with yaml

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const app = express()
 const os = require('os')
+var countReq = 0
 
 app.use(express.json())
 app.use(express.urlencoded({urlencoded: true}))
@@ -9,8 +10,15 @@ console.log(`Kubia server starting...`)
 
 app.get('/*', (req, res, next) => {
     console.log(`Received request from ${req.connection.remoteAddress}`)
-    res.writeHead(200)
-    res.end(`You've hit ${os.hostname()} \n`)
+    countReq++
+    if (countReq > 5 ) {
+        res.status(400)
+        res.end(`Requisitions above the limit`)
+    }
+    else {
+        res.status(200)
+        res.end(`You've hit ${os.hostname()} \n Req count: ${countReq}\n`)
+    }
 })
 
 app.listen(8080, () => {
